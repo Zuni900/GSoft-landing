d21b83a5e47c7a39caccd3aa8868b339e83b2767
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-09-20 16:16:37 +0500

    update performance

---
+++ components/Navbar/data.ts
@@ -0,0 +1,9 @@
+export const pages = [
+  { name: "Company", link: "/company" },
+  { name: "Services", link: "/services" },
+  { name: "Portfolio", link: "/portfolio" },
+  { name: "Blog", link: "/blog" },
+  { name: "Careers", link: "/careers" },
+  { name: "Contact Us", link: "/contactus" },
+  { name: "Life at Gsoft", link: "/lifeatgsoft" },
+];
