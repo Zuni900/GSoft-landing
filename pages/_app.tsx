1dd61aa8d2be6c5bb9c8395dae7f6cf9111d5ba4
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-09-23 10:44:20 +0500

    fix:  path in ts config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { theme } from "../styles/lib/theme/defalutTheme";
+import { theme } from "../styles/theme/defalutTheme";
 import { ThemeProvider } from "@mui/material";
 
 import { createEmotionSsrAdvancedApproach } from "tss-react/nextJs";
